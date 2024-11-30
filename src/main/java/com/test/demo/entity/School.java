a1a7d0fa032eed4636ccf81f0623c20ecc598992
Tuddeepana <tauddeepana@gmail.com>
2024-11-30 22:26:24 +0530

    add the attributes to the Student

--- src/main/java/com/test/demo/entity/School.java
+++ src/main/java/com/test/demo/entity/School.java
@@ -1,4 +1,23 @@
 package com.test.demo.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Table(name = "schools")
+@Entity
+@Data
 public class School extends Base{
+
+    @Column(name = "school_name")
+    private String schoolName;
+
+    @Column(name = "school_address")
+    private String schoolAddress;
+
+    @Column(name = "school_phone")
+    private String schoolPhone;
+
 }
--- src/main/java/com/test/demo/entity/Student.java
+++ src/main/java/com/test/demo/entity/Student.java
@@ -1,4 +1,25 @@
 package com.test.demo.entity;
 
-public class Student {
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "students")
+@Data
+public class Student extends Base {
+
+
+    @Column(name = "student_name")
+    private String studentName;
+
+    @Column(name = "student_address")
+    private String studentAddress;
+
+    @Column(name = "student_phone")
+    private String studentPhone;
+
+
 }
